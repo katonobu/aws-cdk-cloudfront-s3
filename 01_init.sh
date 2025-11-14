1572ea33e5bfe9c25ead1f32a4f32b3248986ebd
Nobuo Kato <katonobu4649@gmail.com>
2025-11-14 02:42:45 +0000

    checkoutから初期環境設定までを更新

--- 01_init.sh
+++ 01_init.sh
@@ -12,41 +12,32 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
+# ここで下記を聞かれる。
+## AWS Access Key ID: IAMユーザーのアクセスキー
+## AWS Secret Access Key: IAMユーザーのシークレットキー
+## Default region name: 例 ap-northeast-1（東京リージョン）
+## Default output format: json（推奨）
+aws configure
+aws configure list
+
 # AWS CDK CLI インストール
 npm install -g aws-cdk
 
 # venv 作成
-python3 -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip
+#python3 -m venv .venv
+#source .venv/bin/activate
+#pip install --upgrade pip
 
 # CDK Python プロジェクト初期化
-mkdir my-static-site && cd my-static-site
-cdk init app --language python
+#mkdir my-static-site && cd my-static-site
+#cdk init app --language python
 
+cd my-static-site
+python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 
 # プロジェクトのvenv環境に入っていることを確認
 pip list
-
-# AWS Solutions Constructs の CloudFront-S3 パターン
-# requiremts.txtを更新したのでインストールされてるはず。
-# pip install aws_solutions_constructs.aws_cloudfront_s3
-
-# 必要なパッケージがインストールされたことを確認
-#pip list
-
-# AWS認証情報を設定（Codespacesに環境変数で設定）
-export AWS_ACCESS_KEY_ID=xxx
-export AWS_SECRET_ACCESS_KEY=xxx
-export AWS_DEFAULT_REGION=ap-northeast-1
-
-# CDK bootstrap（アカウント、リージョンに対して1回やればよい）
-#cdk bootstrap
-
-cdk synth
-
-#cdk deploy
-
-# aws s3 cp ../index.html s3://<your-bucket-name>
+pip install -r requirements.txt
+pip list
--- my-static-site/my_static_site/my_static_site_stack.py
+++ my-static-site/my_static_site/my_static_site_stack.py
@@ -2,9 +2,11 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_s3_deployment as s3deploy,
 )
 from constructs import Construct
 from aws_solutions_constructs.aws_cloudfront_s3 import CloudFrontToS3
+
 
 class MyStaticSiteStack(Stack):
 
@@ -18,4 +20,11 @@ class MyStaticSiteStack(Stack):
         #     self, "MyStaticSiteQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
-        result = CloudFrontToS3(self, 'my_static_site')
+        cfts = CloudFrontToS3(self, 'my_static_site')
+        bucket = cfts.s3_bucket
+
+        # S3へ静的ファイルをデプロイ
+        s3deploy.BucketDeployment(self, "DeployWebsite",
+            sources=[s3deploy.Source.asset("../site-content")],
+            destination_bucket=bucket,
+        )
