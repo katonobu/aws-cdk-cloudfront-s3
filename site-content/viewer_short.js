69846ba46cc4785d7987183cec7e5b91643b4946
Nobuo Kato <katonobu4649@gmail.com>
2025-11-19 13:15:15 +0000

    短期予報共通化

---
+++ site-content/viewer_short.js
@@ -0,0 +1,67 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const location = window.location.search
+    const params = new URLSearchParams(location);
+    const dir = params.get('dir');
+    fetch(`./${dir}/metadata.json`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("ネットワークエラー: " + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const releasedEle = document.getElementById("released");
+            releasedEle.textContent = data["title"]
+
+            const itemContainer = document.getElementById("index_link");
+            items = data["files"]
+            items.forEach(item => {
+                /*
+    {
+      "id": "tanki_yoho",
+      "name": "kaisetsu_tanki.svg",
+      "title": "短期予報解説資料"
+    },
+                */
+                // <li>要素生成
+                const li = document.createElement("li");
+                const a = document.createElement("a");
+                a.href = `#${item.id}`
+                a.textContent = `${item.title}`
+                li.appendChild(a);
+                itemContainer.appendChild(li);
+            });
+            
+            const picturesContainer = document.getElementById("pictures");
+            items.forEach(item => {
+                // <li>要素生成
+                const p = document.createElement("p");
+                const a = document.createElement("a");
+                a.href = `/${dir}/${item.name}`
+                a.target = "_blank";
+                a.id = item.id
+                a.textContent = `${item.title}`
+                p.appendChild(a);
+
+                const img = document.createElement("img");
+                img.style="border: 2px solid black;"
+                img.setAttribute("width", "100%");
+                img.setAttribute("height", "auto");
+                img.src=`/${dir}/${item.name}`
+                p.appendChild(img)
+
+
+                const to_top_a = document.createElement("a");
+                to_top_a.href="#top"
+                to_top_a.textContent="ページトップ"
+                p.appendChild(to_top_a)
+
+                picturesContainer.appendChild(p)
+            });
+
+        })
+        .catch(error => {
+            console.error("データ取得エラー:", error);
+        });
+});
